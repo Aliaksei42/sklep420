89a6370d32e5d930f7b77e65d49e7040e87e6706
Aliaksei Iuko <alexivko01@gmail.com>
2024-04-10 12:51:55 +0200

    Fixed bugs yarn build is ok

--- components/elements/Logo/Logo.tsx
+++ components/elements/Logo/Logo.tsx
@@ -2,7 +2,13 @@ import Link from 'next/link'
 import Image from 'next/image'
 const Logo = () => (
   <Link className='logo' href='/'>
-    <Image className='logo__img' src='/img/logo.svg' alt='Rostelecom Logo' />
+    <Image
+      className='logo__img'
+      src='/img/logo.svg'
+      alt='Rostelecom Logo'
+      width={200}
+      height={50}
+    />
   </Link>
 )
 
